7156e978277131c1aa9ecd875ab5215ec9bdf83f
Eric Kernfeld <eric.kern13@gmail.com>
2015-06-09 15:40:16 -0700

    debug

--- parallel_graph_p3/parallel_graph.cpp
+++ parallel_graph_p3/parallel_graph.cpp
@@ -54,12 +54,12 @@ int main(int argc, char* argv[])
    if(myrank==0)
    {
       master();
-      printf("In main, a master process finished.")
+      printf("In main, a master process finished.");
    }
    else
    {
       slave(myrank);
-      printf("In main, a slave process finished.")
+      printf("In main, a slave process finished.");
    }
    
    // Finalize the MPI session
